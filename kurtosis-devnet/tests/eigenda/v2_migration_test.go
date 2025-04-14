72f4f7c035d5a8d93ac719289a3cf10bca1cba38
Samuel Laferriere <samlaf92@gmail.com>
2025-04-14 20:21:40 +0800

    test(kt-devnet): add extra minute to WaitForBlock to prevent timeout

--- kurtosis-devnet/tests/eigenda/v2_migration_test.go
+++ kurtosis-devnet/tests/eigenda/v2_migration_test.go
@@ -87,7 +87,8 @@ func testEigenDAV2Migration(t *testing.T, v2StageL1BlocksQueriedForBatcherTxs ui
 	stage2FromBlockNum, err := harness.Clients.GethL1Client.BlockNumber(ctxWithTimeout)
 	require.NoError(t, err)
 	stage2ToBlockNum := stage2FromBlockNum + v2StageL1BlocksQueriedForBatcherTxs
-	_, err = geth.WaitForBlock(big.NewInt(int64(stage2ToBlockNum)), harness.Clients.GethL1Client, geth.WithAbsoluteTimeout(v2StageTimeRequired))
+	_, err = geth.WaitForBlock(big.NewInt(int64(stage2ToBlockNum)), harness.Clients.GethL1Client,
+		geth.WithAbsoluteTimeout(v2StageTimeRequired+1*time.Minute)) // add an extra minute to make sure we don't timeout
 	require.NoError(t, err)
 
 	requireBatcherTxsToBeFromLayer(t, stage2FromBlockNum, stage2ToBlockNum, DALayerEigenDAV2, harness.Endpoints.GethL1Endpoint, harness.BatchInboxAddr)
