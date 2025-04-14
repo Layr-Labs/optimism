24d60e805c92e840af67391da581585f87286194
Samuel Laferriere <samlaf92@gmail.com>
2025-04-14 16:36:37 +0800

    test(kt-devnet): skip migration test on holesky to make CI faster
    Copy paste of code comment explaining reasoning:
    	// The issue is that because of the holocene strict ordering rules,
    	// after migrating from v1 to v2, the v2 certs, even though they finalize very quickly
    	// and are returned to op-batcher, op-batcher cannot send them to the batch-inbox until
    	// the v1 certs have been sent. So we need to wait at least 10-15 mins for the v1 certs to be returned
    	// from proxy to op-batcher and submitted to batch-inbox.
    	// starting kurtosis devnet takes ~10 mins and batcher logs tests take 15 mins,
    	// so we are already at 25 mins. Trying to keep CI time under 30 mins,
    	// so we skip this test for now. We could turn on if we had a nice inabox eigenda that would allow
    	// for much faster v1 finalization.

--- kurtosis-devnet/tests/eigenda/v2_migration_test.go
+++ kurtosis-devnet/tests/eigenda/v2_migration_test.go
@@ -11,11 +11,23 @@ import (
 )
 
 func TestEigenDAV2Migration_Memstore(t *testing.T) {
-	testEigenDAV2Migration(t)
+	// 20 is enough in memstore mode since certs finalize instantly and thus land
+	// in batcher-inbox very fast.
+	testEigenDAV2Migration(t, 20)
 }
 
 func TestEigenDAV2Migration_Holesky(t *testing.T) {
-	testEigenDAV2Migration(t)
+	// The issue is that because of the holocene strict ordering rules,
+	// after migrating from v1 to v2, the v2 certs, even though they finalize very quickly
+	// and are returned to op-batcher, op-batcher cannot send them to the batch-inbox until
+	// the v1 certs have been sent. So we need to wait at least 10-15 mins for the v1 certs to be returned
+	// from proxy to op-batcher and submitted to batch-inbox.
+	// starting kurtosis devnet takes ~10 mins and batcher logs tests take 15 mins,
+	// so we are already at 25 mins. Trying to keep CI time under 30 mins,
+	// so we skip this test for now. We could turn on if we had a nice inabox eigenda that would allow
+	// for much faster v1 finalization.
+	t.Skip("skipping test because would make ci take too long. We only run the memstore version in CI.")
+	// testEigenDAV2Migration(t, 200)
 }
 
 // TestEigenDAV2Migration tests a rollup migration from eigenDA V1 to V2.
@@ -24,27 +36,36 @@ func TestEigenDAV2Migration_Holesky(t *testing.T) {
 // This test simply checks that the batcher txs have the correct version byte.
 // The batches inbox transactions are queried via geth's GraphQL API.
 //
+// l1BlocksQueriedForBatcherTxs is the number of blocks to query for batcher txs, for v1 and v2 stages.
+// Need to make sure each stage contains at least 2 commitments of the correct type. This can only happen if channels
+// are being closed in time, which requires either: sending traffic with traffic-generator, or setting a low (e.g. 2 L1 blocks) channel-timeout.
+//
 // Note: because this test modifies the proxy's state config, it should be run in isolation (sequentially).
-func testEigenDAV2Migration(t *testing.T) {
-	// both stages are 20*6 seconds = 2 mins, and we leave 8 mins for op-node finalization
-	testTimeout := 2*2*time.Minute + 8*time.Minute
+func testEigenDAV2Migration(t *testing.T, l1BlocksQueriedForBatcherTxs uint64) {
+	l1BlockTime := 6 * time.Second
+	eachStageTimeRequired := time.Duration(l1BlocksQueriedForBatcherTxs) * l1BlockTime
+	numBatcherStages := 2                          // v1 and v2 stages
+	opNodeFinalizationStageTime := 8 * time.Minute // we leave 8 mins for op-node finalization
+	testTimeout := time.Duration(numBatcherStages)*eachStageTimeRequired + opNodeFinalizationStageTime
 	ctxWithTestTimeout, cancel := context.WithTimeout(context.Background(), testTimeout)
 	defer cancel()
 
 	harness := NewHarness(t)
 
-	// Number of blocks to query for batcher txs, for v1 and v2 stages.
-	// Need to make sure each stage contains at least 2 commitments of the correct type. This can only happen if channels
-	// are being closed in time, which requires either: sending traffic with traffic-generator, or setting a low (e.g. 2 L1 blocks) channel-timeout.
-	l1BlocksQueriedForBatcherTxs := uint64(20)
-
-	// 1. Check that the original commitments are EigenDA V1
-	t.Logf("[Stage1: EigenDA V1] Checking that the initial commitments are EigenDA V1")
 	// explicitly set to v1 since kurtosis can also be started with proxy in v2 dispersal mode
 	ctxWithTimeout, cancel := context.WithTimeout(ctxWithTestTimeout, 5*time.Second)
 	defer cancel()
-	err := harness.Clients.ProxyClients.SetDispersalBackend(ctxWithTimeout, EigenDACertVersionV1)
+	certVersion, err := harness.Clients.ProxyClients.GetDispersalBackend(ctxWithTimeout)
 	require.NoError(t, err)
+	if certVersion != EigenDACertVersionV1 {
+		// V1 certs take a long time to confirm because of batching+bridging,
+		// so we only run this test if the proxy is already in V1 mode,
+		// meaning it has a pipeline of certs that are waiting to be confirmed.
+		t.Skip("skipping test because proxy is not in EigenDA V1 mode")
+	}
+
+	// 1. Check that the original commitments are EigenDA V1
+	t.Logf("[Stage1: EigenDA V1] Checking that the initial commitments are EigenDA V1")
 
 	stage1FromBlockNum := harness.TestStartL1BlockNum
 	stage1ToBlockNum := stage1FromBlockNum + l1BlocksQueriedForBatcherTxs
