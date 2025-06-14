869c01633cb80d0afd3727dadd0b8a2b2e8e687b
BowenX <bxue@eigenlabs.org>
2025-06-13 18:34:48 -0700

    use proxy methods

--- op-e2e/actions/proofs/helpers/eigenda_proxy.go
+++ op-e2e/actions/proofs/helpers/eigenda_proxy.go
@@ -1,66 +1,34 @@
 package helpers
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"time"
 
+	"github.com/Layr-Labs/eigenda-proxy/clients/memconfig_client"
 	actionsHelpers "github.com/ethereum-optimism/optimism/op-e2e/actions/helpers"
 	"github.com/stretchr/testify/require"
 )
-
-// The following type matches the eigenda memstore API in eigenda-proxy
-type proxyStatusCodePayload struct {
-	GetReturnsStatusCode int
-}
 
 func InstructProxyWithGETStatusCodeError(
 	t actionsHelpers.Testing,
 	statusCode int,
 ) error {
 	proxyAddr := "http://127.0.0.1:3100"
-	getTimeout := 5 * time.Second
 
-	var setPayload = proxyStatusCodePayload{
-		GetReturnsStatusCode: statusCode,
-	}
+	client := memconfig_client.New(&memconfig_client.Config{URL: proxyAddr})
 
-	body, err := json.Marshal(setPayload)
-	require.NoError(t, err)
-
-	req, err := http.NewRequestWithContext(t.Ctx(), http.MethodPatch, fmt.Sprintf("%s/memstore/config", proxyAddr), bytes.NewReader(body))
-	// a localhost request should not return error
-	require.NoError(t, err)
-
-	client := &http.Client{Timeout: getTimeout}
-	resp, err := client.Do(req)
-	require.NoError(t, err)
-
-	require.NotEqual(t, resp.StatusCode, http.StatusNotFound)
-
-	// Check it is configured correctly
-	req, err = http.NewRequestWithContext(t.Ctx(), http.MethodGet, fmt.Sprintf("%s/memstore/config/status-code", proxyAddr), nil)
-	require.NoError(t, err)
-	client = &http.Client{Timeout: getTimeout}
-	resp, err = client.Do(req)
-	require.NoError(t, err)
-
-	respBody, err := io.ReadAll(resp.Body)
+	memConfig, err := client.GetConfig(t.Ctx())
 	if err != nil {
-		panic(fmt.Errorf("read response: %w", err))
+		return fmt.Errorf("GetConfig: %w", err)
+	}
+	memConfig.InstructedMode = memconfig_client.InstructedMode{
+		GetReturnsStatusCode: statusCode,
+		IsActivated:          true,
 	}
 
-	fmt.Println("memstore config body", string(respBody))
+	newMemConfig, err := client.UpdateConfig(t.Ctx(), memConfig)
+	require.NoError(t, err)
 
-	var statusCodePayload proxyStatusCodePayload
-	json.Unmarshal(respBody, &statusCodePayload)
-
-	fmt.Println("memstore current status code ", statusCodePayload)
-
-	require.Equal(t, statusCodePayload.GetReturnsStatusCode, statusCode)
+	require.Equal(t, newMemConfig.InstructedMode.GetReturnsStatusCode, statusCode)
 	return nil
 }
 
